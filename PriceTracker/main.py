5dd01d29b1f684e3a639c8028d43c64e7efc21a5
Ritika-007 <91926843+Ritika-007@users.noreply.github.com>
2022-12-21 22:52:54 +0530

    Update main.py

--- PriceTracker/main.py
+++ PriceTracker/main.py
@@ -4,7 +4,7 @@ import lxml
 import smtplib
 
 PRODUCT_URL = "https://www.flipkart.com/vaseline-intensive-care-deep-moisture-body-lotion/p/itm74b8bcc8e4bc8?pid=MSCFKEQ5UYVGXGKS&lid=LSTMSCFKEQ5UYVGXGKS6RMIMX&marketplace=FLIPKART"
-EMAIL = "nehaisarockstar93@gmail.com"
+EMAIL = "prasadritika007@gmail.com"
 PASSWORD = YOUR_EMAIL_PASSWORD
 
 
